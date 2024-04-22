531ef66989a77f777622bcaa60464cf09f1b0367
AmanDas07 <97444854+AmanDas07@users.noreply.github.com>
2024-04-22 17:51:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,16 +14,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 
-app.use(cors({
-    origin: function (origin, callback) {
-        const allowedOrigins = ['https://main--bright-starburst-6cac81.netlify.app', 'http://localhost:3000']; 
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+app.use(
+    cors({
+        allowedHeaders: "*",
+        allowedMethodes: "*",
+        origin: "*",
+    })
+    
+)
 app.use(express.json());
 app.use(morgan("dev"));
 
